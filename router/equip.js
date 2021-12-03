6d97c0086ce01aac4c3e0e9c4ce79ded6a0c71f1
Adhere <291645896@qq.com>
2021-12-03 22:47:08 +0800

    修改设备模块

--- router/equip.js
+++ router/equip.js
@@ -24,12 +24,36 @@ router.get('/getEquip',auth,async(req,res) => {
      else return res.status(404).json({msg:'没有这条记录'})
 })
 
-router.post('/changeEquip',auth,async(req,res) => {
+router.post('/addEquip',auth,async(req,res) => {
     console.log(req.body);
     const r=await equip.create(req.body)
     if(!r)
     return res.status(400).json({msg:'添加设备失败！'})
     res.status(200).json({msg:'添加设备成功！',data:r})
 })
+
+router.post('/addEquip',auth,async(req,res) => {
+  console.log(req.body);
+  const r=await equip.create(req.body)
+  if(!r)
+  return res.status(400).json({msg:'添加设备失败！'})
+  res.status(200).json({msg:'添加设备成功！',data:r})
+})
+
+router.post('/changeEquip',auth,async(req,res) => {
+    console.log(req.body);
+    let id = req.body.id
+    delete req.body.id
+    const r=await equip.update(req.body,{
+      where:{
+        id
+      }
+    })
+    if(!r)
+    return res.status(400).json({msg:'修改设备失败！'})
+    res.status(200).json({msg:'修改设备成功！'})
+})
+
+
 
 module.exports = router
